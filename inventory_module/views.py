facaaa6d4290bd7a12c8361ba3b5268b00535157
Robin-Z <oolocalzh@163.com>
2016-08-26 18:55:48 +0800

    def access_filter() function and refactor inventory()

--- inventory_module/views.py
+++ inventory_module/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from models import user, goods 
 
@@ -7,21 +8,17 @@ from models import user, goods
 def welcome(request):
     return render(request, 'inventory_module/inventory/welcome.html')
 
-def inventory(request):
+def access_filter(request):
     # Receive user name and password from welcome.html page
     userName = request.GET['user_name']
     pwd = request.GET['passwd']
-
+    
     # Judge if user name and password are correct, if correct direct to inventory_page.html, otherwise show user the warning
     if userName != '' and pwd != '':
         try:
             visitUser = user.objects.get(user_name=request.GET['user_name'])
             if pwd == visitUser.user_passwd:
-                
-                # if user name and password is correct, get inventory info from database and show them in inventory_page.html
-                goods_list = goods.objects.all()
-                return render(request, 'inventory_module/inventory/inventory_page.html',{'user_name':userName, 'passwd':pwd, 'goods_list': goods_list})
-
+                return render(request, 'inventory_module/inventory/inventory_page.html',{'user_name':userName, 'passwd':pwd,})
             else:
                 login_warning = 'user name or pwd is incorrect!'
                 return render(request,'inventory_module/inventory/welcome.html',{'login_warning': login_warning})
@@ -31,4 +28,22 @@ def inventory(request):
     else:
         login_warning = 'user name or password cannot be blank!'
         return render(request, 'inventory_module/inventory/welcome.html',{'login_warning': login_warning})
+
+
+def inventory(request):
+    
+    #get inventory info from database and show them in inventory_page.html
+    goods_list = goods.objects.all()
+    paginator = Paginator(goods_list, 1)
+    page = request.GET.get('page')
+    try:
+        goods_per_page = paginator.page(page)
+    except PageNotAnInteger:
+        goods_per_page = paginator.page(1)
+    except EmptyPage:
+        goods_per_page = paginator.page(paginator.num_pages)
+        
+    return render(request, 'inventory_module/inventory/inventory_page.html',{'goods_list': goods_list, 'goods_per_page':goods_per_page})
+
+            
 
