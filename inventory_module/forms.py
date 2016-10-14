eb68b82d6282f2d8cc9f2051a348b5eb0bd05565
Robin-Z <oolocalzh@163.com>
2016-10-14 18:21:22 +0800

    add my_info_form

--- inventory_module/forms.py
+++ inventory_module/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
-
 class borrow_material_form(forms.Form):
     goods_id = forms.IntegerField()
+    goods_name = forms.CharField()
     goods_part_num = forms.CharField()
     goods_spec = forms.CharField()
     goods_revision = forms.CharField()
@@ -12,3 +12,7 @@ class borrow_material_form(forms.Form):
     goods_borrow_qty = forms.IntegerField()
     goods_borrow_date = forms.DateField()
 
+class my_info_form(forms.Form):
+    user_id = forms.IntegerField()
+    user_name = forms.CharField()
+    email = forms.EmailField()
