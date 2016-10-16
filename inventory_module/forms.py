406a106482da713212604cba719809a794b3e7db
Robin-Z <oolocalzh@163.com>
2016-10-16 21:10:43 +0800

    Set text file to readonly

--- inventory_module/forms.py
+++ inventory_module/forms.py
@@ -12,7 +12,26 @@ class borrow_material_form(forms.Form):
     goods_borrow_qty = forms.IntegerField()
     goods_borrow_date = forms.DateField()
 
+    def __init__(self, *args, **kwargs):
+        super(forms.Form, self).__init__(*args, **kwargs)
+        self.fields['goods_id'].widget.attrs['readonly'] = True
+        self.fields['goods_name'].widget.attrs['readonly'] = True
+        self.fields['goods_part_num'].widget.attrs['readonly'] = True
+        self.fields['goods_spec'].widget.attrs['readonly'] = True
+        self.fields['goods_revision'].widget.attrs['readonly'] = True
+        self.fields['goods_location'].widget.attrs['readonly'] = True
+        self.fields['goods_unit'].widget.attrs['readonly'] = True
+        self.fields['goods_onhand_qty'].widget.attrs['readonly'] = True
+        self.fields['goods_borrow_date'].widget.attrs['readonly'] = True
+
+
 class my_info_form(forms.Form):
     user_id = forms.IntegerField()
-    user_name = forms.CharField()
+    user_name = forms.CharField(max_length=254)
     email = forms.EmailField()
+
+    def __init__(self, *args, **kwargs):
+        super(forms.Form, self).__init__(*args,**kwargs)
+        self.fields['user_id'].widget.attrs['readonly'] = True
+        self.fields['user_name'].widget.attrs['readonly'] = True
+        self.fields['email'].widget.attrs['readonly'] = True
