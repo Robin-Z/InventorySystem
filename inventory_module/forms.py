61b9c08910389a4ea88187afa497ee2010d030d4
Robin-Z <oolocalzh@163.com>
2016-10-17 12:54:15 +0800

    Set min_value to 0 of IntegerField
    1. Set min_value to 0 of IntegerField
    2. Use widget to set TextField to readonly

--- inventory_module/forms.py
+++ inventory_module/forms.py
@@ -1,37 +1,18 @@
 from django import forms
 
 class borrow_material_form(forms.Form):
-    goods_id = forms.IntegerField()
-    goods_name = forms.CharField()
-    goods_part_num = forms.CharField()
-    goods_spec = forms.CharField()
-    goods_revision = forms.CharField()
-    goods_location = forms.CharField()
-    goods_unit = forms.CharField()
-    goods_onhand_qty = forms.IntegerField()
-    goods_borrow_qty = forms.IntegerField()
-    goods_borrow_date = forms.DateField()
-
-    def __init__(self, *args, **kwargs):
-        super(forms.Form, self).__init__(*args, **kwargs)
-        self.fields['goods_id'].widget.attrs['readonly'] = True
-        self.fields['goods_name'].widget.attrs['readonly'] = True
-        self.fields['goods_part_num'].widget.attrs['readonly'] = True
-        self.fields['goods_spec'].widget.attrs['readonly'] = True
-        self.fields['goods_revision'].widget.attrs['readonly'] = True
-        self.fields['goods_location'].widget.attrs['readonly'] = True
-        self.fields['goods_unit'].widget.attrs['readonly'] = True
-        self.fields['goods_onhand_qty'].widget.attrs['readonly'] = True
-        self.fields['goods_borrow_date'].widget.attrs['readonly'] = True
-
+    goods_id = forms.IntegerField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    goods_name = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    goods_part_num = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    goods_spec = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    goods_revision = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    goods_location = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    goods_unit = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    goods_onhand_qty = forms.IntegerField(min_value=0, widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    goods_borrow_qty = forms.IntegerField(min_value=0)
+    goods_borrow_date = forms.DateField(widget=forms.DateInput(attrs={'readonly': 'readonly'}))
 
 class my_info_form(forms.Form):
-    user_id = forms.IntegerField()
-    user_name = forms.CharField(max_length=254)
-    email = forms.EmailField()
-
-    def __init__(self, *args, **kwargs):
-        super(forms.Form, self).__init__(*args,**kwargs)
-        self.fields['user_id'].widget.attrs['readonly'] = True
-        self.fields['user_name'].widget.attrs['readonly'] = True
-        self.fields['email'].widget.attrs['readonly'] = True
+    user_id = forms.IntegerField(min_value=0, widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    user_name = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
