6d90e7f1f719e344b7922a74eb900d4ce21fcf6b
Robin-Z <oolocalzh@163.com>
2016-10-18 18:25:32 +0800

    Add logout url
    Redirect to login page when logout

--- inventory_module/urls.py
+++ inventory_module/urls.py
@@ -5,9 +5,11 @@ from django.contrib.auth.views import login, logout
 
 urlpatterns = [
     url(r'^accounts/login/$', login),    # Django build-in login view
+    url(r'^accounts/logout/$', logout, {'next_page': '/inventory_module/accounts/login'}, name='logout'),
     url(r'^inventory_page/$', views.inventory, name='inventory'),
-
     url(r'^my_borrow_list/$', views.my_borrow, name='my_borrow_list'),
+    url(r'^my_info/$', views.my_info, name='my_info'),
     url(r'^borrow_material/(\d+)/$', views.borrow_material, name='borrow_material'),
     url(r'^add_borrow_record/$', views.add_borrow_record, name='add_borrow_record'),
+
 ]
