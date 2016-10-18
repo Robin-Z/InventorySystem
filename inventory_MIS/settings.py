34a61ca36b98343707315b61675a052ebc0d712c
Robin-Z <oolocalzh@163.com>
2016-10-18 18:35:30 +0800

    Add LOGIN_URL

--- inventory_MIS/settings.py
+++ inventory_MIS/settings.py
@@ -107,4 +107,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_URL = '/inventory_module/accounts/'
 LOGIN_REDIRECT_URL = '/inventory_module/inventory_page/'
