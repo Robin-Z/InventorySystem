2d30f05dc285879a968cebfe0ca99cad20bf0c59
Robin-Z <oolocalzh@163.com>
2016-11-05 15:22:42 +0800

    Update urls.py as updated templates
    1. redirect to home page when logged in
    2. custom login page

--- inventory_module/urls.py
+++ inventory_module/urls.py
@@ -1,15 +1,15 @@
-
 from django.conf.urls import url
 from . import views
 from django.contrib.auth.views import login, logout
+from .forms import LoginForm
 
 urlpatterns = [
-    url(r'^accounts/login/$', login),    # Django build-in login view
+    url(r'^accounts/login/$', login, {'template_name': 'registration/login.html', 'authentication_form': LoginForm}), # Django build-in login view
     url(r'^accounts/logout/$', logout, {'next_page': '/inventory_module/accounts/login'}, name='logout'),
+    url(r'^home/$', views.home_page, name='home_page'),
     url(r'^inventory_page/$', views.inventory, name='inventory'),
     url(r'^my_borrow_list/$', views.my_borrow, name='my_borrow_list'),
     url(r'^my_info/$', views.my_info, name='my_info'),
     url(r'^borrow_material/(\d+)/$', views.borrow_material, name='borrow_material'),
     url(r'^add_borrow_record/$', views.add_borrow_record, name='add_borrow_record'),
-
 ]
