51c34ec5e603c2280e8653aacc25362f20675fa7
Robin-Z <oolocalzh@163.com>
2016-11-06 15:17:29 +0800

    Add default login url

--- inventory_MIS/urls.py
+++ inventory_MIS/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import login
+from inventory_module.forms import LoginForm
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^inventory_module/', include('inventory_module.urls')),
+    url(r'^$', login, {'authentication_form': LoginForm}),
 
 ]
