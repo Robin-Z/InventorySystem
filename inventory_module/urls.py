1408dd468638b359afdcfd32cbe1bd229f70beff
Robin-Z <oolocalzh@163.com>
2016-11-09 13:33:40 +0800

    Add url to about page and contact page
    Add url to about page and contact page
    Add query_by_key_word url to new view function query_by_keyword

--- inventory_module/urls.py
+++ inventory_module/urls.py
@@ -4,7 +4,8 @@ from django.contrib.auth.views import login, logout
 from .forms import LoginForm
 
 urlpatterns = [
-    url(r'^accounts/login/$', login, {'template_name': 'registration/login.html', 'authentication_form': LoginForm}), # Django build-in login view
+    # Django build-in login view
+    url(r'^accounts/login/$', login, {'template_name': 'registration/login.html', 'authentication_form': LoginForm}),
     url(r'^accounts/logout/$', logout, {'next_page': '/inventory_module/accounts/login'}, name='logout'),
     url(r'^home/$', views.home_page, name='home_page'),
     url(r'^inventory_page/$', views.inventory, name='inventory'),
@@ -12,4 +13,7 @@ urlpatterns = [
     url(r'^my_info/$', views.my_info, name='my_info'),
     url(r'^borrow_material/(\d+)/$', views.borrow_material, name='borrow_material'),
     url(r'^add_borrow_record/$', views.add_borrow_record, name='add_borrow_record'),
+    url(r'^query_by_keyword/$', views.query_by_keyword, name='query_by_keyword'),
+    url(r'^about/$', views.about_page, name='about_page'),
+    url(r'^contact/$', views.contact_page, name='contact_page'),
 ]
