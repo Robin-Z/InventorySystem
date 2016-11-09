b4e2f41affe0111338f7c660a6d5c2372dddec1a
Robin-Z <oolocalzh@163.com>
2016-11-09 13:46:37 +0800

    Add comments

--- inventory_module/forms.py
+++ inventory_module/forms.py
@@ -1,12 +1,27 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 
-# If you don't do this you cannot use Bootstrap CSS
+
+"""
+Customized build-in login form in LoginForm, otherwise you cannot use Bootstrap CSS.
+"""
+
+
 class LoginForm(AuthenticationForm):
     username = forms.CharField(label="Username", max_length=30,
-                               widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'username', 'placeholder': 'User name'}))
+                               widget=forms.TextInput(attrs={'class': 'form-control',
+                                                             'name': 'username',
+                                                             'placeholder': 'User name'}))
     password = forms.CharField(label="Password", max_length=30,
-                               widget=forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password', 'placeholder': 'Passowrd'}))
+                               widget=forms.PasswordInput(attrs={'class': 'form-control',
+                                                                 'name': 'password',
+                                                                 'placeholder': 'Passowrd'}))
+
+
+"""
+Customized borrow material form
+"""
+
 
 class borrow_material_form(forms.Form):
     goods_id = forms.IntegerField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
@@ -20,7 +35,15 @@ class borrow_material_form(forms.Form):
     goods_borrow_qty = forms.IntegerField(min_value=0)
     goods_borrow_date = forms.DateField(widget=forms.DateInput(attrs={'readonly': 'readonly'}))
 
+
+"""
+Use Django form to customize my info form
+"""
+
+
 class my_info_form(forms.Form):
-    user_id = forms.IntegerField(min_value=0, widget=forms.TextInput(attrs={'readonly': 'readonly', 'outline': 'medium'}))
-    user_name = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly', 'outline': 'medium'}))
+    user_id = forms.IntegerField(min_value=0, widget=forms.TextInput(attrs={'readonly': 'readonly',
+                                                                            'outline': 'medium'}))
+    user_name = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly',
+                                                              'outline': 'medium'}))
     email = forms.EmailField(widget=forms.TextInput(attrs={'readonly': 'readonly', 'outline': 'medium'}))
