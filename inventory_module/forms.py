761b51eaa8af7b4d21b8b554d50f5d7ddc206bd6
Robin-Z <oolocalzh@163.com>
2016-11-14 15:38:42 +0800

    add CSS class to customized form
    Add 'class': 'input-lg' to my_info_form and borrow_material_form

--- inventory_module/forms.py
+++ inventory_module/forms.py
@@ -24,16 +24,17 @@ Customized borrow material form
 
 
 class borrow_material_form(forms.Form):
-    goods_id = forms.IntegerField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
-    goods_name = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
-    goods_part_num = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
-    goods_spec = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
-    goods_revision = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
-    goods_location = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
-    goods_unit = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
-    goods_onhand_qty = forms.IntegerField(min_value=0, widget=forms.TextInput(attrs={'readonly': 'readonly'}))
-    goods_borrow_qty = forms.IntegerField(min_value=0)
-    goods_borrow_date = forms.DateField(widget=forms.DateInput(attrs={'readonly': 'readonly'}))
+    goods_id = forms.IntegerField(widget=forms.TextInput(attrs={'readonly': 'readonly', 'class': 'input-lg'}))
+    goods_name = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly', 'class': 'input-lg'}))
+    goods_part_num = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly', 'class': 'input-lg'}))
+    goods_spec = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly', 'class': 'input-lg'}))
+    goods_revision = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly', 'class': 'input-lg'}))
+    goods_location = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly', 'class': 'input-lg'}))
+    goods_unit = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly', 'class': 'input-lg'}))
+    goods_onhand_qty = forms.IntegerField(min_value=0, widget=forms.TextInput(attrs={'readonly': 'readonly',
+                                                                                     'class': 'input-lg'}))
+    goods_borrow_qty = forms.IntegerField(min_value=0, widget=forms.NumberInput(attrs={'class': 'input-lg'}))
+    goods_borrow_date = forms.DateField(widget=forms.DateInput(attrs={'readonly': 'readonly', 'class': 'input-lg'}))
 
 
 """
@@ -43,7 +44,7 @@ Use Django form to customize my info form
 
 class my_info_form(forms.Form):
     user_id = forms.IntegerField(min_value=0, widget=forms.TextInput(attrs={'readonly': 'readonly',
-                                                                            'outline': 'medium'}))
+                                                                            'class': 'input-lg'}))
     user_name = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly',
-                                                              'outline': 'medium'}))
-    email = forms.EmailField(widget=forms.TextInput(attrs={'readonly': 'readonly', 'outline': 'medium'}))
+                                                              'class': 'input-lg'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'readonly': 'readonly', 'class': 'input-lg'}))
